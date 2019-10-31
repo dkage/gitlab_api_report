850cb26479e2c4bc649d347d8bd9a921033320e5
Danilo Cunha <danilokage@gmail.com>
2019-10-31 19:37:04 -0300

    First commit, uploading initial script

---
+++ json_handler.py
@@ -0,0 +1,28 @@
+import json
+
+
+# https://10.68.13.200:8888/api/v4/projects/24/repository/commits?per_page=900
+with open('commits.json', 'r') as json_file:
+    json_commits = json.load(json_file)
+
+
+html_output = "<html><body>"
+for element in json_commits:
+    if "Merge branch" in element['message'] or "Merge remote" in element['message']:
+        continue
+    html_output += "<div>\n"
+    html_output += "<h2>" + element["title"] + "</h2>\n"
+    html_output += "<h3>Commit: " + element["short_id"] + "</h3>\n"
+    message_line_breaks = element["message"].replace("\n", "<br />\n")
+    html_output += "<p>" + element["message"] + "</p>"
+    html_output += "<p></p>"
+    html_output += "</div><br><br><br>\n"
+
+html_output += "</body></html>"
+print(html_output)
+
+
+json_file.close()
+html_output_file = open("output.html", "w")
+html_output_file.write(html_output)
+html_output_file.close()
